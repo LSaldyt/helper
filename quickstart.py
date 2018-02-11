de43d6281517a1a8274f228007180e4c87d1fd44
LSaldyt <lucassaldyt@gmail.com>
2018-02-11 13:39:08 -0700

    Creates time-gap calculation

--- quickstart.py
+++ quickstart.py
@@ -1,115 +1,42 @@
-from __future__ import print_function
-import httplib2
-import os
+import datetime, time
+from lib import get_service, get_events
 
-from apiclient import discovery
-from oauth2client import client
-from oauth2client import tools
-from oauth2client.file import Storage
+def get_time(event, loc):
+    s = event[loc].get('dateTime', event[loc].get('date'))
+    return s
 
-import datetime
-
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
-
-# If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/calendar-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/calendar'
-CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Google Calendar API Python Quickstart'
-
-
-def get_credentials():
-    """Gets valid user credentials from storage.
-
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
-    """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'calendar-python-quickstart.json')
-
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
-
-def create_event(service):
-    event = {
-      'summary': 'Google I/O 2015',
-      'location': '800 Howard St., San Francisco, CA 94103',
-      'description': 'A chance to hear more about Google\'s developer products.',
-      'start': {
-        'dateTime': '2018-02-11T09:00:00-07:00',
-        'timeZone': 'America/Los_Angeles',
-      },
-      'end': {
-        'dateTime': '2018-02-11T17:00:00-07:00',
-        'timeZone': 'America/Los_Angeles',
-      },
-      'recurrence': [
-        'RRULE:FREQ=DAILY;COUNT=2'
-      ],
-      'attendees': [
-        {'email': 'lpage@example.com'},
-        {'email': 'sbrin@example.com'},
-      ],
-      'reminders': {
-        'useDefault': False,
-        'overrides': [
-          {'method': 'email', 'minutes': 24 * 60},
-          {'method': 'popup', 'minutes': 10},
-        ],
-      },
-    }
-
-    event = service.events().insert(calendarId='primary', body=event).execute()
-    return event
-
+def get_hour(event, loc):
+    s = get_time(event, loc)
+    time = s[11:-6]
+    return datetime.datetime.strptime(time, '%X')
 
 def main():
-    """Shows basic usage of the Google Calendar API.
-
-    Creates a Google Calendar API service object and outputs a list of the next
-    10 events on the user's calendar.
-    """
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('calendar', 'v3', http=http)
-
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    eventsResult = service.events().list(
-        calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
-        orderBy='startTime').execute()
-    events = eventsResult.get('items', [])
+    service = get_service()
+    events = get_events(service, n=20)
 
     if not events:
         print('No upcoming events found.')
     for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
+        start = get_time(event, 'start')
         print(start, event['summary'])
 
-    events = create_event(service)
-    print(events)
+    if len(events) > 0:
+        first = events[0]
+        now = datetime.datetime.strptime(time.strftime('%X'), '%X')
+        gap = get_hour(first, 'start') - now
+        print('Gap until: ' first['summary'])
+        print('{} minutes'.format(gap.total_seconds() / 60))
 
+    for a, b in zip(events[:-1], events[1:]):
+        aEnd   = get_hour(a, 'end')
+        bStart = get_hour(b, 'start')
+        gap = bStart - aEnd
 
+        print('Gap after: ', a['summary'])
+        print('{} minutes'.format(gap.total_seconds() / 60))
+
+    #events = create_event(service)
+    #print(events)
 
 if __name__ == '__main__':
     main()
